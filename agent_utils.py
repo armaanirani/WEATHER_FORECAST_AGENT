285bbd0449969effd8b44def6cd0d415d50b566f
Armaan Irani <armaanirani@gmail.com>
2025-04-26 10:33:13 +0530

    created and configured the weather agent and tool

---
+++ agent_utils.py
@@ -0,0 +1,70 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+from pydantic import BaseModel
+from pydantic_ai import Agent, RunContext
+from pydantic_ai.settings import ModelSettings
+
+
+# Loading the environment variables from the .env file
+load_dotenv()
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+OWM_API_KEY = os.getenv("OWM_API_KEY")
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+# Defining the output schema
+class WeatherForecast(BaseModel):
+    location: str
+    temperature: float
+    description: str
+    wind_speed: float
+    humidity: float
+    pressure: float
+    visibility: float
+    cloudiness: float
+    sunrise: str
+    sunset: str
+    timezone: str
+
+# Defining the agent
+weather_agent = Agent(
+    model='groq:llama-3.3-70b-versatile',
+    model_settings=ModelSettings(
+        temperature=0.2
+    ),
+    output_type=str,
+    system_prompt="""You are a weather forecast agent. 
+    Use the get_weather_forecast tool to return the weather forecast for that location."""
+)
+
+# Weather forecast tool
+@weather_agent.tool
+def get_weather_forecast(ctx: RunContext, city: str) -> WeatherForecast:
+    """
+    This function returns the current weather forecast for the given city.
+    """
+    params = {
+        "q": city,
+        "appid": OWM_API_KEY,
+        "units": 'metric'
+    }
+    
+    response = requests.get(BASE_URL, params=params).json()
+    
+    return WeatherForecast(
+        location=response['name'],
+        temperature=response['main']['temp'],
+        description=response['weather'][0]['description'],
+        wind_speed=response['wind']['speed'],
+        humidity=response['main']['humidity'],
+        pressure=response['main']['pressure'],
+        visibility=response['visibility'],
+        cloudiness=response['clouds']['all'],
+        sunrise=response['sys']['sunrise'],
+        sunset=response['sys']['sunset'],
+        timezone=response['timezone']
+    )
+
+
